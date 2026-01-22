ef90ea4f0e70546a72205f9908773982fb22ec2c
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-01-21 21:20:20 -0600

    feat: add home link to recipe not found banner

--- website/src/features/recipes/components/recipe-not-found-banner/recipe-not-found-banner.component.tsx
+++ website/src/features/recipes/components/recipe-not-found-banner/recipe-not-found-banner.component.tsx
@@ -1,3 +1,11 @@
+import { ActionBanner } from "../action-banner/action-banner.component";
+
 export function RecipeNotFoundBanner({}: {}) {
-  return <p>Recipe not found in the cloud ...</p>;
+  return (
+    <ActionBanner
+      to="/"
+      message="Recipe not found in the cloud ..."
+      linkText="Go home"
+    />
+  );
 }
