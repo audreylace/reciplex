70d640bb4cc62e897fabf679235e74f3e63c4bfa
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-02-15 17:46:53 -0600

    make button row more reusable

--- website/src/features/core/components/form-buttons/form-buttons.component.tsx
+++ website/src/features/core/components/form-buttons/form-buttons.component.tsx
@@ -1,6 +1,16 @@
 import type { PropsWithChildren } from "preact/compat";
 import formStyles from "../../form-common/form-common.module.css";
 
-export function FormButtons({ children }: PropsWithChildren<{}>) {
-  return <div className={formStyles.formButtonRow}>{children}</div>;
+export function FormButtons({
+  children,
+  notInForm,
+}: PropsWithChildren<{ notInForm?: boolean }>) {
+  return (
+    <div
+      className={formStyles.formButtonRow}
+      data-not-in-form={notInForm ?? false}
+    >
+      {children}
+    </div>
+  );
 }
