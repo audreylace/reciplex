816f17b161b98730312e2d7bbc19a83a3a745d96
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-02-24 22:07:15 -0600

    change icon

--- website/src/features/recipes/components/offline-banner/offline-banner.component.tsx
+++ website/src/features/recipes/components/offline-banner/offline-banner.component.tsx
@@ -4,6 +4,7 @@ export function OfflineBanner() {
   return (
     <InformationBanner
       title="Offline"
+      icon="bi bi-wifi-off"
       message="Your offline. We will reconnect once you are online again."
     />
   );
