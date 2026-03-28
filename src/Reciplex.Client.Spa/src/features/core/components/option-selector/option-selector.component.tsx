3973e745c411917c0864543844cd24343a6f065b
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-03-28 18:08:18 -0500

    feat: reusable option selector

---
+++ src/Reciplex.Client.Spa/src/features/core/components/option-selector/option-selector.component.tsx
@@ -0,0 +1,63 @@
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from "@headlessui/react";
+
+import componentStyles from "./option-selector.module.css";
+import { SuccessButton } from "../buttons/success-button.component";
+
+export function OptionSelector<T extends IOptionEntry>({
+  options,
+  value,
+  onChange,
+}: IOptionSelectorProps<T>) {
+  return (
+    <Listbox value={value} onChange={(newValue) => onChange(newValue)}>
+      <ListboxButton as={SuccessButton} buttonType="dotted">
+        <div className={componentStyles.textWrapper}>
+          <div className={componentStyles.visibleValue}>{value.name}</div>
+          {options.map((sz) => (
+            <div
+              key={sz.key}
+              role="structure"
+              aria-hidden
+              tabIndex={-1}
+              className={componentStyles.hiddenValue}
+            >
+              {sz.name}
+            </div>
+          ))}
+        </div>
+      </ListboxButton>
+      <ListboxOptions
+        className={componentStyles.dropDownOptionContainer}
+        anchor="bottom"
+      >
+        {options.map((sz) => (
+          <ListboxOption
+            className={componentStyles.dropDownOption}
+            key={sz.key}
+            value={sz}
+          >
+            {sz.name}
+          </ListboxOption>
+        ))}
+      </ListboxOptions>
+    </Listbox>
+  );
+}
+
+export interface IOptionEntry {
+  /** the entries unique id */
+  key: string;
+  /** the entries name */
+  name: string;
+}
+
+export interface IOptionSelectorProps<T extends IOptionEntry> {
+  value: T;
+  options: T[];
+  onChange: (value: T) => void;
+}
