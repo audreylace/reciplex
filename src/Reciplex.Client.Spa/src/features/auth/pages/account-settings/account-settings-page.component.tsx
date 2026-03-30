44d5547aaa447e7b161a052bf76519f918f50bec
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-03-29 21:12:08 -0500

    finish users features

--- src/Reciplex.Client.Spa/src/features/auth/pages/account-settings/account-settings-page.component.tsx
+++ src/Reciplex.Client.Spa/src/features/auth/pages/account-settings/account-settings-page.component.tsx
@@ -1,29 +1,27 @@
 /* eslint-disable react-hooks/incompatible-library */
-import {
-  Field,
-  Fieldset,
-  Input,
-  Label,
-  Legend,
-  Switch,
-} from "@headlessui/react";
+import { Field, Fieldset, Input, Label, Legend } from "@headlessui/react";
 import formCommonStylesModule from "../../../core/form-common/form-common.module.css";
 import { AuthenticatedRouteGuard } from "../../components/authenticated-route-guard/authenticated-route-guard.component";
-import { useGetAccountsQuery } from "../../hooks/useGetAccountsQuery.hook";
 import type { IHttpUserJson } from "../../http-clients/users-http-client";
 import { SuccessButton } from "../../../core/components/buttons/success-button.component";
 import { DangerButton } from "../../../core/components/buttons/danger-button.component";
 import { FormButtons } from "../../../core/components/form-buttons/form-buttons.component";
-import { useState } from "preact/hooks";
 import { useForm } from "react-hook-form";
 import accountSettingsPageStylesModule from "./account-settings-page.module.css";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { ApplicationErrorBanner } from "../../../core/components/banner/application-error-banner.component";
 import {
   ErrorBanner,
   InformationBanner,
+  SuccessBanner,
 } from "../../../core/components/banner/banner.component";
-import { OfflineBanner } from "../../../core/components/banner/offline-banner.component";
+import { useUpdateAccountMutation } from "../../hooks/useUpdateAccountMutation.hook";
+import { ActionFailedTryAgainCancel } from "../../../recipes/components/action-failed-try-again-cancel/action-failed-try-again-cancel.component";
+import { makeBookListPath } from "../../../recipes/route-utils";
+import { useAuthClients } from "../../hooks/useAuthClients.hook";
+import { useAsyncResult } from "../../../core/hooks/useAsyncResult.hook";
+import { useState } from "preact/hooks";
+import { useDeleteAccountMutation } from "../../hooks/useDeleteAccountMutation.hook";
 
 export function AccountSettingsPage() {
   const { accountKey } = useParams<{ accountKey: string }>();
@@ -35,21 +33,208 @@ export function AccountSettingsPage() {
   return (
     <main className={formCommonStylesModule.formMain}>
       <AuthenticatedRouteGuard>
-        <AccountSettingBody accountKey={accountKey} />
+        <PageBody accountKey={accountKey} />
       </AuthenticatedRouteGuard>
     </main>
   );
 }
 
-function DeleteAccountForm({ account }: { account: IHttpUserJson }) {
-  const { register, watch, formState } = useForm<{
+function PageBody({ accountKey }: { accountKey: string }) {
+  const { usersClient } = useAuthClients();
+  const accountsQuery = useAsyncResult({
+    asyncAction: () => usersClient.getAccounts({ noCache: true }),
+  });
+
+  const [hideDelete, setHideDelete] = useState(false);
+  const [hideSettings, setHideSettings] = useState(false);
+
+  let account: IHttpUserJson | undefined;
+  switch (accountsQuery.state) {
+    case "error":
+    default:
+      return <ApplicationErrorBanner />;
+
+    case "pending":
+      return <InformationBanner title="Fetching accounts" />;
+
+    case "success":
+      account = accountsQuery.data.find((a) => a.userKey === accountKey);
+      if (!account) {
+        return (
+          <ErrorBanner
+            title="Not Found"
+            message="Requested account was not found"
+            buttonCaption="Select Account"
+            to={accountSelectPath}
+          ></ErrorBanner>
+        );
+      }
+
+      return (
+        <div className={accountSettingsPageStylesModule.wrapper}>
+          {!hideSettings && (
+            <AccountSettingsForm
+              account={account}
+              onActionOccurred={() => setHideDelete(true)}
+            />
+          )}
+          {!hideDelete && (
+            <DeleteAccountForm
+              account={account}
+              onActionOccurred={() => setHideSettings(true)}
+            />
+          )}
+        </div>
+      );
+  }
+}
+
+function AccountSettingsForm({
+  account,
+  onActionOccurred,
+}: {
+  account: IHttpUserJson;
+  onActionOccurred: () => void;
+}) {
+  const { register, formState, handleSubmit } = useForm<{
+    displayName: string;
+  }>({
+    defaultValues: {
+      displayName: account.displayName,
+    },
+  });
+  const accountMutation = useUpdateAccountMutation();
+  const navigate = useNavigate();
+  const navigateAccountSelect = useSelectAccountNavigate();
+
+  if (accountMutation.status === "pending") {
+    return <InformationBanner title="saving changes" />;
+  }
+
+  if (accountMutation.status === "error") {
+    return (
+      <ActionFailedTryAgainCancel
+        message="failed to save changes to account"
+        cancelCaption="go back"
+        cancelAction={navigateAccountSelect}
+      />
+    );
+  }
+
+  if (accountMutation.status === "success") {
+    return (
+      <SuccessBanner
+        title="Account Updated"
+        message="Account settings were saved."
+        buttonCaption="View Books"
+        onButtonClick={() => navigate(makeBookListPath())}
+      />
+    );
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        onActionOccurred();
+        await accountMutation.mutateAsync({
+          userKey: account.userKey,
+          displayName: data.displayName,
+          concurrencyToken: account.concurrencyTag,
+        });
+      })}
+    >
+      <Fieldset className={formCommonStylesModule.fieldSet}>
+        <Legend className={formCommonStylesModule.formLegend}>
+          Account Settings for {account.displayName} ({account.userKey})
+        </Legend>
+        <Field className={formCommonStylesModule.inputGroup}>
+          <Label className={formCommonStylesModule.label}>Display Name</Label>
+          <Input
+            type="text"
+            className={formCommonStylesModule.fieldControl}
+            required
+            maxLength={64}
+            placeholder={"Provide a Display Name"}
+            {...register("displayName", {
+              required: true,
+              maxLength: 64,
+            })}
+          ></Input>
+          {formState.errors.displayName?.type === "required" && (
+            <span>
+              Provide a display name so others can know what to call you
+            </span>
+          )}
+          {formState.errors.displayName?.type === "maxLength" && (
+            <span>Display name must be no longer than {64} characters</span>
+          )}
+        </Field>
+      </Fieldset>
+      <FormButtons>
+        <SuccessButton type="submit">Save</SuccessButton>
+      </FormButtons>
+    </form>
+  );
+}
+
+function DeleteAccountForm({
+  account,
+  onActionOccurred,
+}: {
+  account: IHttpUserJson;
+  onActionOccurred: () => void;
+}) {
+  const { register, watch, formState, handleSubmit } = useForm<{
     displayName: string;
     enabled: boolean;
   }>();
 
   const showForm = watch("enabled");
+
+  const deleteAccountMutation = useDeleteAccountMutation();
+  const navigateAccountSelect = useSelectAccountNavigate();
+
+  if (deleteAccountMutation.status === "pending") {
+    return <InformationBanner title="deleting account..." />;
+  }
+
+  if (deleteAccountMutation.status === "error") {
+    return (
+      <ActionFailedTryAgainCancel
+        message="failed to delete account"
+        cancelCaption="go back"
+        cancelAction={navigateAccountSelect}
+      />
+    );
+  }
+
+  if (deleteAccountMutation.status === "success") {
+    return (
+      <SuccessBanner
+        title="Account Deleted"
+        message="Account was deleted."
+        buttonCaption="View Accounts"
+        onButtonClick={navigateAccountSelect}
+      />
+    );
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit((data) => {
+        if (!data.enabled) {
+          return;
+        }
+        if (data.displayName !== account.displayName) {
+          return;
+        }
+        onActionOccurred();
+        deleteAccountMutation.mutateAsync({
+          userKey: account.userKey,
+          concurrencyToken: account.concurrencyTag,
+        });
+      })}
+    >
       <Fieldset className={formCommonStylesModule.fieldSet}>
         <Legend className={formCommonStylesModule.formLegend}>
           Delete Account
@@ -97,88 +282,17 @@ function DeleteAccountForm({ account }: { account: IHttpUserJson }) {
           </>
         )}
       </Fieldset>
-      {showForm && <DangerButton type="submit">Delete Account</DangerButton>}
+      {showForm && (
+        <FormButtons>
+          <DangerButton type="submit">Delete Account</DangerButton>
+        </FormButtons>
+      )}
     </form>
   );
 }
+const accountSelectPath = "/accounts/-/select";
 
-function AccountSettingsForm({ account }: { account: IHttpUserJson }) {
-  const { register, formState } = useForm<{
-    displayName: string;
-  }>();
-
-  return (
-    <form>
-      <Fieldset className={formCommonStylesModule.fieldSet}>
-        <Legend className={formCommonStylesModule.formLegend}>
-          Account Settings for {account.displayName} ({account.userKey})
-        </Legend>
-        <Field className={formCommonStylesModule.inputGroup}>
-          <Label className={formCommonStylesModule.label}>Display Name</Label>
-          <Input
-            type="text"
-            className={formCommonStylesModule.fieldControl}
-            required
-            maxLength={64}
-            placeholder={"Provide a Display Name"}
-            value={account.displayName}
-            {...register("displayName", {
-              required: true,
-              maxLength: 64,
-            })}
-          ></Input>
-          {formState.errors.displayName?.type === "required" && (
-            <span>
-              Provide a display name so others can know what to call you
-            </span>
-          )}
-          {formState.errors.displayName?.type === "maxLength" && (
-            <span>Display name must be no longer than {64} characters</span>
-          )}
-        </Field>
-      </Fieldset>
-      <FormButtons>
-        <SuccessButton>Save</SuccessButton>
-        <DangerButton>Cancel</DangerButton>
-      </FormButtons>
-    </form>
-  );
-}
-
-function AccountSettingsForms({ account }: { account: IHttpUserJson }) {
-  return (
-    <div className={accountSettingsPageStylesModule.wrapper}>
-      <AccountSettingsForm account={account} />
-      <DeleteAccountForm account={account} />
-    </div>
-  );
-}
-
-function AccountSettingBody({ accountKey }: { accountKey: string }) {
-  const accountListQuery = useGetAccountsQuery(true);
-
-  let account: IHttpUserJson | undefined;
-  switch (accountListQuery.status) {
-    case "error":
-    default:
-      return <ApplicationErrorBanner />;
-    case "pending":
-      if (accountListQuery.fetchStatus === "paused") {
-        return <OfflineBanner />;
-      }
-      return <InformationBanner title="Fetching accounts" />;
-    case "success":
-      account = accountListQuery.data.find((a) => a.userKey === accountKey);
-      if (!account) {
-        return (
-          <ErrorBanner
-            title="Not Found"
-            message="Requested account was not found"
-            buttonCaption="Select Account"
-            to="/accounts/-/select"
-          ></ErrorBanner>
-        );
-      }
-      return <AccountSettingsForms account={account} />;
-  }
+function useSelectAccountNavigate() {
+  const navigate = useNavigate();
+  return () => navigate(accountSelectPath);
 }
