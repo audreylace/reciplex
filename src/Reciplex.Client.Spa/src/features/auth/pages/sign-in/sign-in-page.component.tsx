b92556520bafa3ac1b0b5ccf6ae7f81b117e588c
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-04-05 21:16:35 -0500

    bug

--- src/Reciplex.Client.Spa/src/features/auth/pages/sign-in/sign-in-page.component.tsx
+++ src/Reciplex.Client.Spa/src/features/auth/pages/sign-in/sign-in-page.component.tsx
@@ -1,23 +1,9 @@
 import { useEffect } from "preact/hooks";
-import { useNavigate } from "react-router";
 import { useSelectAccountNavigate } from "../../hooks/useSelectAccountNavigate.hook";
-import { useNeedChallengeQuery } from "../../hooks/useNeedChallengeQuery.hook";
 import { AuthenticatedRouteGuard } from "../../components/authenticated-route-guard/authenticated-route-guard.component";
 
 /** Page that either shows the challenge UI or redirects the user to the account select page */
 export function SignInPage() {
-  const challengeQuery = useNeedChallengeQuery();
-  const navigate = useNavigate();
-  const goToSelect = useSelectAccountNavigate()[1];
-
-  useEffect(() => {
-    if (challengeQuery.status === "success" && !challengeQuery.data) {
-      goToSelect({
-        replace: true, // back button should go back to the page that summoned us
-      });
-    }
-  }, [challengeQuery.data, challengeQuery.status, goToSelect, navigate]);
-
   return (
     <AuthenticatedRouteGuard allowNullUser>
       <Redirect />
