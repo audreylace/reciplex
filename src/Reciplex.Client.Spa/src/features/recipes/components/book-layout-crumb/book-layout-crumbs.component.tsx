0751b8233354634c065c6fc56707bf2a3d5db6f5
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-05-05 07:29:48 -0500

    DRY layout

---
+++ src/Reciplex.Client.Spa/src/features/recipes/components/book-layout-crumb/book-layout-crumbs.component.tsx
@@ -0,0 +1,64 @@
+import { NavLink } from "react-router";
+import { useGetRecipeBookById } from "../../hooks/useGetRecipeBookById.hook";
+import { useGetRecipeByIdQuery } from "../../hooks/useGetRecipeByIdQuery.hook";
+import {
+  makeBookListPath,
+  makeViewRecipeBookPath,
+  makeViewRecipePath,
+} from "../../route-utils";
+import Typography from "@mui/material/Typography";
+import bookLayoutCrumbsStyleModule from "./book-layout-crumbs.module.css";
+
+export function BookLayoutCrumbs({ bookId, recipeId }: IBookLayoutCrumbsProps) {
+  const bookQuery = useGetRecipeBookById(bookId);
+  const recipeQuery = useGetRecipeByIdQuery(recipeId);
+
+  return (
+    <ul className={bookLayoutCrumbsStyleModule.crumbTrail}>
+      <li className={bookLayoutCrumbsStyleModule.crumbElement}>
+        <NavLink to={makeBookListPath()}>
+          <span className={bookLayoutCrumbsStyleModule.bookCrumb}>
+            <Typography variant="subtitle1">Books</Typography>
+          </span>
+        </NavLink>
+      </li>
+      {bookId && (
+        <>
+          <CrumbDivider />
+          <li className={bookLayoutCrumbsStyleModule.crumbElement}>
+            <NavLink to={makeViewRecipeBookPath(bookId ?? "")}>
+              <span className={bookLayoutCrumbsStyleModule.bookCrumb}>
+                <Typography variant="subtitle1">
+                  {bookQuery.data?.name ?? ""}
+                </Typography>
+              </span>
+            </NavLink>
+          </li>
+        </>
+      )}
+      {recipeId && bookId && recipeQuery.data && (
+        <>
+          <CrumbDivider />
+          <li className={bookLayoutCrumbsStyleModule.crumbElement}>
+            <NavLink to={makeViewRecipePath(bookId, recipeId)}>
+              <span className={bookLayoutCrumbsStyleModule.recipeCrumb}>
+                <Typography variant="subtitle1">
+                  {recipeQuery.data?.name ?? ""}
+                </Typography>
+              </span>
+            </NavLink>
+          </li>
+        </>
+      )}
+    </ul>
+  );
+}
+
+export interface IBookLayoutCrumbsProps {
+  bookId?: string;
+  recipeId?: string;
+}
+
+function CrumbDivider() {
+  return <li className={bookLayoutCrumbsStyleModule.crumbDivider}>/</li>;
+}
--- src/Reciplex.Client.Spa/src/features/recipes/layouts/book-layout/book-layout.component.tsx
+++ src/Reciplex.Client.Spa/src/features/recipes/layouts/book-layout/book-layout.component.tsx
@@ -1,123 +1,18 @@
-import { NavLink, Outlet, useNavigate, useParams } from "react-router";
-import { useGetRecipeBookById } from "../../hooks/useGetRecipeBookById.hook";
-import { useGetRecipeByIdQuery } from "../../hooks/useGetRecipeByIdQuery.hook";
-import {
-  makeBookListPath,
-  makeViewRecipeBookPath,
-  makeViewRecipePath,
-} from "../../route-utils";
+import { Outlet, useParams } from "react-router";
 import { AuthenticatedRouteGuard } from "../../../auth/components/authenticated-route-guard/authenticated-route-guard.component";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Box from "@mui/material/Box";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
-import Slide from "@mui/material/Slide";
-import styles from "./book-layout.module.css";
-import { useSignInNavigate } from "../../../auth/hooks/useSignInNavigate.hook";
+import { BookLayoutNavigationBar } from "../../components/book-layout-navigation-bar/book-layout-navigation-bar.component";
 
 export function BookLayout() {
+  const { bookId, recipeId } = useParams<{
+    bookId?: string;
+    recipeId?: string;
+  }>();
   return (
     <AuthenticatedRouteGuard>
-      <BookLayoutBar2 fixed />
-      <Box sx={{ visibility: "hidden" }}>
-        <BookLayoutBar2 fixed={false} />
-      </Box>
+      <BookLayoutNavigationBar bookId={bookId} recipeId={recipeId} />
       <main className="pageMain">
         <Outlet />
       </main>
     </AuthenticatedRouteGuard>
   );
-}
-
-function BookLayoutBar2({ fixed }: { fixed: boolean }) {
-  const navigate = useNavigate();
-  const { bookId, recipeId } = useParams<{
-    bookId?: string;
-    recipeId?: string;
-  }>();
-  const bookQuery = useGetRecipeBookById(bookId);
-  const recipeQuery = useGetRecipeByIdQuery(recipeId);
-  const signInNavigate = useSignInNavigate()[1];
-
-  const trigger = useScrollTrigger({
-    target: window,
-  });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      <AppBar position={fixed ? "fixed" : "static"}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ mr: 3 }}>
-            <Button
-              onClick={() => navigate("/")}
-              sx={{
-                color: "white",
-                display: "block",
-                textDecoration: "none",
-              }}
-            >
-              <Typography variant="subtitle1">Reciplex</Typography>
-            </Button>
-          </Typography>
-
-          <ul className={styles.crumbTrail}>
-            <li className={styles.crumbElement}>
-              <NavLink to={makeBookListPath()}>
-                <span className={styles.bookCrumb}>
-                  <Typography variant="subtitle1">Books</Typography>
-                </span>
-              </NavLink>
-            </li>
-            {bookId && (
-              <>
-                <CrumbDivider />
-                <li className={styles.crumbElement}>
-                  <NavLink to={makeViewRecipeBookPath(bookId ?? "")}>
-                    <span className={styles.bookCrumb}>
-                      <Typography variant="subtitle1">
-                        {bookQuery.data?.name ?? ""}
-                      </Typography>
-                    </span>
-                  </NavLink>
-                </li>
-              </>
-            )}
-            {recipeId && bookId && recipeQuery.data && (
-              <>
-                <CrumbDivider />
-                <li className={styles.crumbElement}>
-                  <NavLink to={makeViewRecipePath(bookId, recipeId)}>
-                    <span className={styles.recipeCrumb}>
-                      <Typography variant="subtitle1">
-                        {recipeQuery.data?.name ?? ""}
-                      </Typography>
-                    </span>
-                  </NavLink>
-                </li>
-              </>
-            )}
-          </ul>
-
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            onClick={() => {
-              signInNavigate();
-            }}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Slide>
-  );
-}
-
-function CrumbDivider() {
-  return <li className={styles.crumbDivider}>/</li>;
 }
