e109f58709d06f0973eac9a0f268378bb9526b08
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-05-24 11:43:44 -0500

    chore: https for testing; config fixes;

--- src/Reciplex.Client.Spa/vite.config.ts
+++ src/Reciplex.Client.Spa/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({
-      presets: [reactCompilerPreset()],
+    basicSsl({
+      /** name of certification */
+      name: "test",
+      /** custom trust domains */
+      domains: ["example.com"],
+      /** optional, days before certificate expires */
+      ttlDays: 30,
     }),
   ],
   build: {},
