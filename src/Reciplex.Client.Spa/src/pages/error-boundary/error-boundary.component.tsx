86fa73a37f69ac51b7262511edc2b09f18a1012b
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-07-12 20:40:19 -0500

    fix jsx issue

--- src/Reciplex.Client.Spa/src/pages/error-boundary/error-boundary.component.tsx
+++ src/Reciplex.Client.Spa/src/pages/error-boundary/error-boundary.component.tsx
@@ -1,11 +1,11 @@
 import Backdrop from "@mui/material/Backdrop";
-import {  HardApplicationFailureBanner } from "../../features/core/components/hard-application-failure-banner/hard-application-failure-banner.component";
+import { HardApplicationFailureBanner } from "../../features/core/components/hard-application-failure-banner/hard-application-failure-banner.component";
 
 /** router error boundary component */
 export function ErrorBoundary() {
   return (
     <Backdrop open>
-      <HardApplicationFailureBanner>
+      <HardApplicationFailureBanner />
     </Backdrop>
   );
 }
