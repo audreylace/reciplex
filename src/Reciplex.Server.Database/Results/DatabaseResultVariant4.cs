2915076f974c84beb6949a47d8bfe575fa843e9b
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-08-02 10:51:55 -0500

    chore: doc database result variant 4

--- src/Reciplex.Server.Database/Results/DatabaseResultVariant4.cs
+++ src/Reciplex.Server.Database/Results/DatabaseResultVariant4.cs
@@ -1,23 +1,61 @@
 namespace Reciplex.Server.Database.Results;
 
-public class DatabaseResultVariant<T0, T1, T2, T3> : IDatabaseResult
-    where T0 : IDatabaseResult
-    where T1 : IDatabaseResult
-    where T2 : IDatabaseResult
-    where T3 : IDatabaseResult
+/// <summary>
+/// Database result holding 4 types
+/// </summary>
+/// <typeparam name="TValue1">value type 1</typeparam>
+/// <typeparam name="TValue2">value type 2</typeparam>
+/// <typeparam name="TValue3">value type 3</typeparam>
+/// <typeparam name="TValue4">value type 4</typeparam>
+public class DatabaseResultVariant<TValue1, TValue2, TValue3, TValue4> : IDatabaseResult
+    where TValue1 : IDatabaseResult
+    where TValue2 : IDatabaseResult
+    where TValue3 : IDatabaseResult
+    where TValue4 : IDatabaseResult
 {
+    /// <summary>
+    /// Private constructor, can only be built through operators
+    /// </summary>
+    /// <param name="result">the value to store</param>
     private DatabaseResultVariant(IDatabaseResult result)
     {
         Result = result;
     }
 
+    /// <summary>
+    /// The held result variant
+    /// </summary>
     public IDatabaseResult Result { get; private init; }
 
-    public static implicit operator DatabaseResultVariant<T0, T1, T2, T3>(T0 t) => new(t);
+    /// <summary>
+    /// Builds this from <paramref name="value1"/>
+    /// </summary>
+    /// <param name="value1">the value to box</param>
+    public static implicit operator DatabaseResultVariant<TValue1, TValue2, TValue3, TValue4>(
+        TValue1 value1
+    ) => new(value1);
 
-    public static implicit operator DatabaseResultVariant<T0, T1, T2, T3>(T1 t) => new(t);
+    /// <summary>
+    /// Builds this from <paramref name="value2"/>
+    /// </summary>
+    /// <param name="value2">the value to box</param>
+    public static implicit operator DatabaseResultVariant<TValue1, TValue2, TValue3, TValue4>(
+        TValue2 value2
+    ) => new(value2);
 
-    public static implicit operator DatabaseResultVariant<T0, T1, T2, T3>(T2 t) => new(t);
+    /// <summary>
+    /// Builds this from <paramref name="value3"/>
+    /// </summary>
+    /// <param name="value3">the value to box</param>
+    public static implicit operator DatabaseResultVariant<TValue1, TValue2, TValue3, TValue4>(
+        TValue3 value3
+    ) => new(value3);
 
-    public static implicit operator DatabaseResultVariant<T0, T1, T2, T3>(T3 t) => new(t);
+    /// <summary>
+    /// Builds this from <paramref name="value4"/>
+    /// </summary>
+    /// <param name="value4">the value to box</param>
+    public static implicit operator DatabaseResultVariant<TValue1, TValue2, TValue3, TValue4>(
+        TValue4 value4
+    ) => new(value4);
 }
