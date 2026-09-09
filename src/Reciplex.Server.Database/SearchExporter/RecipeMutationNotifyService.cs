5e794f30e74aa7a7541e30f4407420a047b22a92
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-09-08 20:20:00 -0500

    tweaks

--- src/Reciplex.Server.Database/SearchExporter/RecipeMutationNotifyService.cs
+++ src/Reciplex.Server.Database/SearchExporter/RecipeMutationNotifyService.cs
@@ -6,7 +6,7 @@ namespace Reciplex.Server.Database.SearchExporter;
 public class RecipeMutationNotifyService(IOptions<SearchExporterOptions> options)
     : IRecipeMutationNotifyService
 {
-    private readonly Channel<long> _wakeChannel = Channel.CreateBounded<long>(
+    private readonly Channel<long> _changeChannel = Channel.CreateBounded<long>(
         new BoundedChannelOptions(32)
         {
             SingleReader = true,
@@ -14,13 +14,50 @@ public class RecipeMutationNotifyService(IOptions<SearchExporterOptions> options
         }
     );
 
+    private readonly Channel<long> _newChannel = Channel.CreateBounded<long>(
+        new BoundedChannelOptions(32)
+        {
+            SingleReader = true,
+            FullMode = BoundedChannelFullMode.DropWrite,
+        }
+    );
+
+    public bool DrainUpToChange(int count)
+    {
+        int counter = 0;
+        while (_changeChannel.Reader.TryRead(out _) && counter < count)
+        {
+            counter++;
+        }
+        return counter > 0;
+    }
+
+    public bool DrainUpToNew(int count)
+    {
+        int counter = 0;
+        while (_newChannel.Reader.TryRead(out _) && counter < count)
+        {
+            counter++;
+        }
+        return counter > 0;
+    }
+
+    public void NotifyNew()
+    {
+        if (!options.Value.Enable)
+        {
+            return;
+        }
+        _newChannel.Writer.TryWrite(0);
+    }
+
     public void NotifyOne()
     {
         if (!options.Value.Enable)
         {
             return;
         }
-        _wakeChannel.Writer.TryWrite(0);
+        _changeChannel.Writer.TryWrite(0);
     }
 
     public async Task WaitForChange(CancellationToken ct)
@@ -30,9 +67,26 @@ public class RecipeMutationNotifyService(IOptions<SearchExporterOptions> options
             throw new InvalidOperationException();
         }
 
-        while (!ct.IsCancellationRequested && await _wakeChannel.Reader.WaitToReadAsync(ct))
+        while (!ct.IsCancellationRequested && await _changeChannel.Reader.WaitToReadAsync(ct))
         {
-            if (_wakeChannel.Reader.TryRead(out _))
+            if (_changeChannel.Reader.TryRead(out _))
+            {
+                return;
+            }
+            ct.ThrowIfCancellationRequested();
+        }
+    }
+
+    public async Task WaitForNew(CancellationToken ct)
+    {
+        if (!options.Value.Enable)
+        {
+            throw new InvalidOperationException();
+        }
+
+        while (!ct.IsCancellationRequested && await _newChannel.Reader.WaitToReadAsync(ct))
+        {
+            if (_changeChannel.Reader.TryRead(out _))
             {
                 return;
             }
