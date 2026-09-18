dd58d2aaa915bd0231945db154024fbdf6e483dc
Audrey Lace Taylor <audrey.lace.taylor@gmail.com>
2026-09-17 21:23:30 -0500

    clean-up

--- src/Reciplex.Server.Database/SearchExporter/HostedServices/NewRecipeSearchRowExporterHostedService.cs
+++ src/Reciplex.Server.Database/SearchExporter/HostedServices/NewRecipeSearchRowExporterHostedService.cs
@@ -7,18 +7,28 @@ using Reciplex.Server.Database.SearchExporter.Repositories;
 
 namespace Reciplex.Server.Database.SearchExporter.HostedServices;
 
+/// <summary>
+/// Finds and exports new recipes
+/// </summary>
+/// <param name="searchRepo">repository for search status records in the database</param>
+/// <param name="tagProvider">provides random tags</param>
+/// <param name="notificationService">notifies when new recipes are created and ready for export</param>
+/// <param name="exporterStrategy">strategy for marshalling records to the search index</param>
+/// <param name="options">options for the service</param>
+/// <param name="logger">service logger</param>
 class NewRecipeSearchRowExporterHostedService(
-    IRecipeSearchExportStatusRepository recipeSearchExportStatusRepository,
+    IRecipeSearchExportStatusRepository searchRepo,
     IConcurrencyTagProvider tagProvider,
-    IRecipeMutationNotifyService recipeMutationNotifyService,
-    RecipeSearchIndexExporterStrategy recipeSearchIndexExporterStrategy,
+    IRecipeMutationNotifyService notificationService,
+    RecipeSearchIndexExporterStrategy exporterStrategy,
     IOptions<SearchExporterOptions> options,
     ILogger<NewRecipeSearchRowExporterHostedService> logger
 ) : BackgroundService
 {
+    /// <inheritdoc />
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        if (!options.Value.Enable)
+        if (!options.Value.Enable) // exit if search is not enabled
         {
             return;
         }
@@ -28,47 +38,38 @@ class NewRecipeSearchRowExporterHostedService(
         {
             try
             {
-                while (!stoppingToken.IsCancellationRequested)
-                {
-                    string leaseToken = tagProvider.NextTag();
-                    var list = await recipeSearchExportStatusRepository.CreateSearchStatusRowsAsync(
-                        options.Value.SearchExportBatchSize,
-                        leaseToken,
-                        leaseExpireTimeSeconds,
-                        stoppingToken
-                    );
-
-                    if (list.Count < 1)
-                    {
-                        break;
-                    }
-
-                    await recipeSearchIndexExporterStrategy.ExportRecipesAsync(
-                        list,
-                        leaseToken,
-                        stoppingToken
-                    );
-                }
-            }
-            catch (Exception ex)
-                when (ex is not OperationCanceledException || !stoppingToken.IsCancellationRequested
-                )
-            {
-                logger.Error_NewRecipeExportLoopFailed(ex);
-                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
-            }
-
-            try
-            {
-                await recipeMutationNotifyService.WaitForNew(
-                    TimeSpan.FromSeconds(30),
+                string leaseToken = tagProvider.NextTag();
+                var list = await searchRepo.CreateSearchStatusRowsAsync(
+                    options.Value.SearchExportBatchSize,
+                    leaseToken,
+                    leaseExpireTimeSeconds,
                     stoppingToken
                 );
+                if (list.Count < 1)
+                {
+                    await notificationService.WaitForNew(TimeSpan.FromSeconds(30), stoppingToken);
+                }
+                else
+                {
+                    await exporterStrategy.ExportRecipesAsync(list, leaseToken, stoppingToken);
+                }
             }
-            catch (OperationCanceledException) when (!stoppingToken.IsCancellationRequested) { }
-            catch (Exception ex) when (ex is not OperationCanceledException)
+            catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
             {
-                logger.Error_NewRecipeExportLoopChannelFailed(ex);
+                return; // exit background service
+            }
+            catch (Exception ex)
+            {
+                logger.Error_NewRecipeExportLoopFailed(ex);
+                try
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+                }
+                catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+                {
+                    return; // exit background service
+                }
+                catch (Exception) { }
             }
         }
     }
